97eda68376c6b7df07a4858fadb1a64837eb5c3d
ADIBA NOOR <adiba.noor003@gmail.com>
2022-09-30 20:15:40 +0530

    added navigation bar

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,18 +17,41 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     final _userPreference = Provider.of<UserViewModel>(context);
     return Scaffold(
+      backgroundColor: Color(0xfffbefe3),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+           children: [
+             InkWell(
+                 onTap: (){
+                   _userPreference.remove().then((value){
+                     Navigator.pushNamed(context, RouteNames.login);
+                   });
+                 },
+                 child: Text("Logout",
+                   style: TextStyle(color: Colors.black87),)),
+           ],
+        ),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          InkWell(
-              onTap: (){
-                _userPreference.remove().then((value){
-                  Navigator.pushNamed(context, RouteNames.login);
-                });
-              },
-              child: Text("Logout"))
+
         ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        items: [
+          Icon(Icons.contacts_outlined, size: 35,color: Colors.red,),
+          Icon(Icons.add,size: 35,color: Colors.red),
+          Icon(Icons.person, size: 35 ,color: Colors.red),
+        ],
+        backgroundColor: Color(0xfffbefe3),
+        color: Colors.amber,
+        animationDuration: Duration(milliseconds: 300),
       ),
     );
 
